8806c0d03da71f208e14593bcd0ac777830a5b1c
David Litmark <david@litmark.se>
2011-06-05 22:32:40 +0200

    Added logic for reading posts from tumblr using the tumblr API

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
-var viewportmeta = document.querySelectorAll('meta[name="viewport"]')[0];
-if (viewportmeta) {
-viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0';
-document.body.addEventListener('gesturestart', function() {
-viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
-}, false);
+    var viewportmeta = document.querySelectorAll('meta[name="viewport"]')[0];
+    if (viewportmeta) {
+        viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0';
+        document.body.addEventListener('gesturestart', function() {
+            viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
+        }, false);
+    }
 }
-}
+
+// read blog posts from tumblr
+// very temporary code. To be refactored and made into a module
+var tumblr = tumblr_api_read;
+var latestPost = tumblr.posts[0];
+var regularTitle = latestPost['regular-title'];
+var url = latestPost.url;
+var regularBody = latestPost['regular-body'].substring(0, 300) + " ...";
+$("body > header").after('' +
+        '<section id="feature">' +
+        '   <header><h2>Featured Blog Post</h2></header>' +
+        '   <div class="entry-content">' +
+        '       <h3><a href="' + url + '" >' + regularTitle + '</a></h3><p>' + regularBody + '</p>' +
+        '   </div>' +
+        '</section>');
