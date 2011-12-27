e815207593c3fff9734df7f62f341c215a9834f3
David Litmark <david@litmark.se>
2011-12-28 00:46:20 +0100

    Temporarily removed logic while converting everything into amd modules

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) 
     var viewportmeta = document.querySelectorAll('meta[name="viewport"]')[0];
     if (viewportmeta) {
         viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0';
-        document.body.addEventListener('gesturestart', function() {
+        document.body.addEventListener('gesturestart', function () {
             viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
         }, false);
     }
@@ -11,23 +11,25 @@ if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) 
 //// zootools
 //// TODO: Refactor!
 //// TODO: zb included twice below?
-(function(d, t) {
-    var zb = d.createElement(t),s = d.getElementsByTagName(t)[0];
-    zb.async = 1;
-    zb.src = 'http://zootool.com/badge/davidlitmark/?count=10&size=75';
-    s.parentNode.insertBefore(zb, s);
-    $("#whoami").before('' +
-        '<section id="zootool">' +
-        '   <header><h2>Bookmarks</h2></header>' +
-        '   <div class="entry-content"><div id="zootool-badge"> ' + zb + '</div></div>' +
-        '</section>');
-}(document, 'script'));
+/*(function (d, t) {
+ var zb = d.createElement(t), s = d.getElementsByTagName(t)[0];
+ zb.async = 1;
+ zb.src = 'http://zootool.com/badge/davidlitmark/?count=10&size=75';
+ s.parentNode.insertBefore(zb, s);
+ $("#whoami").before('' +
+ '<section id="zootool">' +
+ '   <header><h2>Bookmarks</h2></header>' +
+ '   <div class="entry-content"><div id="zootool-badge"> ' + zb + '</div></div>' +
+ '</section>');
+ }(document, 'script'));*/
 
 // underscore template syntax
-_.templateSettings = {
-    interpolate : /\{\{(.+?)\}\}/g,
-    evaluate: /<%(.+?)%>/g
-};
+/*
+ _.templateSettings = {
+ interpolate:/\{\{(.+?)\}\}/g,
+ evaluate:/<%(.+?)%>/g
+ };
+ */
 
 
 //// todo: improve!
@@ -40,25 +42,27 @@ _.templateSettings = {
 //});
 
 
-window.App = window.App || {};
+//window.App = window.App || {};
+//
+//App.start = function () {
+//    new App.Router();
+//    Backbone.history.start();
+//};
+//
+//App.Templates = {
+//    twitter:function () {
+//        return "<p>{{ text }}</p><p class='date'>{{ created_at }}</p>";
+//    },
+//    versionhistory:function () {
+//        return "<header><h3>Release {{ release }}</h3></header><ul><% _.each(items,function(item){ %><li>{{ item.text }}</li><% }); %></ul>";
+//    },
+//    latestversion:function () {
+//        return "<ul><% _.each(items,function(item){ %><li>{{ item.text }}</li><% }); %></ul>";
+//    }
+//};
 
-App.start = function() {
-    new App.Router();
-    Backbone.history.start();
-};
+//App.start();
 
-App.Templates = {
-    twitter: function() {
-        return "<p>{{ text }}</p><p class='date'>{{ created_at }}</p>";
-    },
-    versionhistory: function() {
-        return "<header><h3>Release {{ release }}</h3></header><ul><% _.each(items,function(item){ %><li>{{ item.text }}</li><% }); %></ul>";
-    },
-    latestversion: function() {
-        return "<ul><% _.each(items,function(item){ %><li>{{ item.text }}</li><% }); %></ul>";
-    }
-};
 
-App.start();
 
 
